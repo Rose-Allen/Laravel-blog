1b90226a2fb7c52f90b936467d706d9796d11691
unknown <sam_07a@mail.ru>
2022-07-29 04:56:40 +0600

    Добавил реализацию поста и дорабатываю посты сделал: загрузку фото, добавил контент, загрузка фото на бд, привязку с категориями

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -7,6 +7,15 @@
             <!-- Add icons to the links using the .nav-icon class
                  with font-awesome or any other icon font library -->
             <li class="nav-item">
+                <a href="{{route('admin.post.index')}}" class="nav-link">
+{{--                    <i class=" far fa-clipboard"></i>--}}
+                    <i class="nav-icon fas fa-clipboard-list"></i>
+                    <p>
+                        Посты
+                    </p>
+                </a>
+            </li>
+            <li class="nav-item">
                 <a href="{{route('admin.category.index')}}" class="nav-link">
                     <i class="nav-icon fas fa-list"></i>
                     <p>
--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
     <!-- Ionicons -->
     <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
 
@@ -59,6 +60,10 @@
 <script src="{{asset('plugins/jquery/jquery.min.js')}} "></script>
 <!-- jQuery UI 1.11.4 -->
 <script src="{{asset('plugins/jquery-ui/jquery-ui.min.js')}} "></script>
+
+<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}} "></script>
+
+<script src="{{asset('plugins/summernote/summernote-bs4.min.js')}} "></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
     $.widget.bridge('uibutton', $.ui.button)
@@ -66,8 +71,31 @@
 
 <script src="{{asset('plugins/moment/moment.min.js')}} "></script>
 <script src="{{asset('plugins/daterangepicker/daterangepicker.js')}} "></script>
+<script src="{{asset('plugins/bs-custom-file-input/bs-custom-file-input.min.js')}} "></script>
 <script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}} "></script>
 <script src="{{asset('dist/js/adminlte.js')}}"></script>
-
+<script>
+    $(document).ready(function() {
+        $('#summernote').summernote({
+            toolbar: [
+                // [groupName, [list of button]]
+                ['style', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['strikethrough', 'superscript', 'subscript']],
+                ['fontsize', ['fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['height', ['height']]
+            ]
+        });
+    });
+    $(function () {
+        bsCustomFileInput.init();
+    });
+</script>
+<style>
+    .custom-file-input:lang(en)~.custom-file-label::after{
+        content:"...";
+    }
+</style>
 </body>
 </html>
