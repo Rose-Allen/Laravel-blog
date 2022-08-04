939afbb8503068f5100135a2ca319e53fb7cd41d
Dimash <d.esengeldiev@mail.ru>
2022-08-04 14:53:00 +0600

    Личный кабинет юзера

---
+++ resources/views/personal/comment/index.blade.php
@@ -0,0 +1,68 @@
+@extends('personal.layouts.main')
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Комментарии</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item active">Комментарии</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+{{--            <div class="row mt-3">--}}
+{{--                <div class="col-md-4">--}}
+{{--                    <div class="card">--}}
+{{--                        <div class="card-body table-responsive p-0">--}}
+{{--                            <table class="table table-hover text-nowrap">--}}
+{{--                                <thead>--}}
+{{--                                <tr>--}}
+{{--                                    <th>ID</th>--}}
+{{--                                    <th>Название</th>--}}
+{{--                                    <th>Действия</th>--}}
+{{--                                </tr>--}}
+{{--                                </thead>--}}
+{{--                                <tbody>--}}
+{{--                                @foreach($posts as $post)--}}
+{{--                                    <tr>--}}
+{{--                                        <td>{{$post->id}}</td>--}}
+{{--                                        <td>{{$post->title}}</td>--}}
+{{--                                        <td class="table-buttons">--}}
+{{--                                            <a class="btn btn-success" href="{{route('admin.post.show', $post->id)}}"><i class="fas fa-eye"></i></a>--}}
+{{--                                            <form action="{{route('personal.liked.delete', $post->id)}}" method="post">--}}
+{{--                                                @csrf--}}
+{{--                                                @method('DELETE')--}}
+{{--                                                <button type="submit" class="btn btn-danger">--}}
+{{--                                                    <i class="fas fa-trash"></i>--}}
+{{--                                                </button>--}}
+{{--                                            </form>--}}
+{{--                                    </tr>--}}
+{{--                                @endforeach--}}
+{{--                                </tbody>--}}
+{{--                            </table>--}}
+{{--                        </div>--}}
+{{--                        <!-- /.card-body -->--}}
+{{--                    </div>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+            <!-- /.row -->
+
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
