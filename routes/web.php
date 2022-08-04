37e6b35d840ce569d57e6c9f139269451c0b2d33
Dimash <d.esengeldiev@mail.ru>
2022-08-04 14:35:51 +0600

    Доработал админ-панель

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::group(['namespace'=> 'Main'], function (){
 //Admin
 Route::group(['namespace'=>'Admin', 'prefix'=>'admin', 'middleware'=>['auth','admin','verified']], function (){
     Route::group(['namespace'=> 'Main'], function (){
-        Route::get('/', "IndexController");
+        Route::get('/', "IndexController")->name('admin.main.index');
     });
 
     Route::group(['namespace'=> 'Post', 'prefix'=>'posts'], function (){
